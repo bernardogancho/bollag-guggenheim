6452075ba2b3d481f9e7e4c0fce0f27262ffdf80
Bernardo Gancho <bernardogancho99@gmail.com>
2026-03-30 18:08:14 +0100

    Refine site layout and content sections

--- src/_data/brands.js
+++ src/_data/brands.js
@@ -1,14 +1,11 @@
 module.exports = [
   { name: "Closed", slug: "closed" },
-  { name: "Coccinelle", slug: "coccinelle" },
   { name: "Codello", slug: "codello" },
   { name: "Drykorn", slug: "drykorn" },
   { name: "G-Lab", slug: "g-lab" },
   { name: "GUESS", slug: "guess" },
   { name: "iBlues", slug: "iblues" },
-  { name: "Malina", slug: "malina" },
   { name: "More & More", slug: "more-and-more" },
-  { name: "Puntododici", slug: "puntododici" },
   { name: "Rich & Royal", slug: "rich-royal" },
   { name: "YAYA", slug: "yaya" },
   { name: "0039 Italy", slug: "0039-italy" }
